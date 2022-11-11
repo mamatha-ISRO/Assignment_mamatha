84b4e6cae008a6409c076c33a0b1406cc4c5e6b5
mamatha-ISRO <59817698+mamatha-ISRO@users.noreply.github.com>
2022-11-11 13:42:15 -0700

    cups swap and album controlled code added

---
+++ abcalbums/Controllers/AlbumsController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.ComponentModel.DataAnnotations;
+using Nancy.Json;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+
+namespace abcalbums.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlbumsController : ControllerBase
+    {
+        // This getAlbums will get any album that contains the search string in it's title
+        // or all albums if there is no search string
+        [HttpGet]
+        public async Task<List<AlbumFormat>> getAlbums(string searchString)
+        {
+            // If there is no search string return the whole list of albums
+            if (searchString == null)
+            {
+                // Using HttpClient to get the albums from JSONplaceholder.typicode.com 
+                using (HttpClient client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(@"https://jsonplaceholder.typicode.com");
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    string storageString = await client.GetStringAsync("/albums");
+
+                    var albumArray = JsonConvert.DeserializeObject<List<AlbumFormat>>(storageString);
+
+
+                    return albumArray;
+                }
+            }
+            // If there is a search string, get the list of albums and filter for albums with the search string in their title
+            else
+            {
+                // Using HttpClient to get the albums from JSONplaceholder.typicode.com 
+                using (HttpClient client = new HttpClient())
+                {
+                    string uriString = "https://jsonplaceholder.typicode.com";
+
+                    client.BaseAddress = new Uri(uriString);
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    // store the gathered information in storageString until we can filter it out.
+                    string storageString = await client.GetStringAsync("/albums");
+
+                    var albumArray = JsonConvert.DeserializeObject<List<AlbumFormat>>(storageString);
+
+
+                    // Filter the array of albums by the search query (case insensitive)
+
+                    //{
+                    // Create a new list of AlbumFormat so that albums that contain the searchString can be added to it during the filtering process
+                    var filteredAlbumArray = new List<AlbumFormat>();
+
+
+                    // This foreach loop detects whether the seachString is contained in the title of the album
+                    // THIS DOES MEAN THAT IF YOU SEARCH FOR SOMETHING THAT IS CONTAINED IN A LARGER WORD, THE LARGER WORD WILL ALSO BE FILTERED IN
+                    //(i.e. search "we" and "welcome" gets filtered in)
+                    foreach (AlbumFormat album in albumArray)
+                    {
+                        bool stringPresent = album.title.IndexOf(searchString, StringComparison.OrdinalIgnoreCase) >= 0;
+                        if (stringPresent == true)
+                        {
+                            // If the searchString is present in the title then add that album to the filtered array
+                            filteredAlbumArray.Add(album);
+                        }
+                    }
+                    //}
+                    // Return the filtered array to the user
+                    return filteredAlbumArray;
+                }
+            }
+        }
+
+
+        public class AlbumFormat
+        {
+            public string userId { get; set; }
+            public string id { get; set; }
+            public string title { get; set; }
+
+        }
+    }
+}
---
+++ abcalbums/Controllers/CupsController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace abcalbums.Controllers
+{
+    
+   
+        [Route("api/[controller]")]
+        [ApiController]
+        public class CupsController : ControllerBase
+        {
+
+            [HttpGet("swap")]
+            public object Swap([FromBody] IEnumerable<string> SwapCommands)
+            {
+                // The ball starts in the position of 'B'
+                var ballLocation = "B";
+
+                // Read the list of swap commands
+
+                // Start with the first command (loop until all swaps have been read/swaps have been made)
+                //{
+                foreach (string swap in SwapCommands)
+                {
+                    // Check if it is a valid swap
+                    if (swap == "AB" || swap == "BA")
+                    {
+                        // If so, preform the swap
+                        if (ballLocation == "A") { ballLocation = "B"; }
+                        else if (ballLocation == "B") { ballLocation = "A"; }
+                    }
+                    else if (swap == "BC" || swap == "CB")
+                    {
+                        // If so, preform the swap
+                        if (ballLocation == "B") { ballLocation = "C"; }
+                        else if (ballLocation == "C") { ballLocation = "B"; }
+                    }
+                    else if (swap == "AC" || swap == "CA")
+                    {
+                        // If so, preform the swap
+                        if (ballLocation == "A") { ballLocation = "C"; }
+                        else if (ballLocation == "C") { ballLocation = "A"; }
+                    }
+                    else
+                    {
+                        // If it is not a valid swap return BadRequest message
+                        //{
+
+                        // If any swap contains more/less than two characters return a BadRequest with the appropriate message
+                        if (swap.Length != 2) { return BadRequest("The swap command is not in the appropriate format of two characters (Swap command length error)"); }
+
+                        // If any swap has lowercase letters then return a BadRequest with the appropriate message
+                        // This needs to be placed before the Invalid char error, otherwise it won't be triggered (it is more specific than invalid char error)
+                        foreach (char swapChar in swap)
+                        {
+                            if (Char.IsLower(swapChar)) { return BadRequest("There is a lowercase character in a swap command (Lowercase error)"); }
+                        }
+
+                        // If any swap contains characters that are not A, B or C return a BadRequest with the appropriate message
+                        foreach (char swapChar in swap)
+                        {
+                            // Using the ASCII values of capital letters A, B and C (65, 66 and 67 respectively)
+                            // to determine if any of the characters in the swap request are invalid
+                            if ((int)swapChar < 65 || (int)swapChar > 67) { return BadRequest("There is an invalid character in the swap commands (Invalid char error)"); }
+                        }
+
+                        // If any swap is with itself (repeating cup name) return a BadRequest with the appropriate message
+                        if (swap == "AA" || swap == "BB" || swap == "CC") { return BadRequest("The swap can't occur with only one cup (Identical cup error)"); }
+
+
+                        // UNKNOWN ERROR message, in case there is a non-valid swap that the above don't catch (shouldn't be triggered unless the BadRequest checks miss something)
+                        BadRequest("UNKNOWN ERROR -- Invalid swap request");
+
+                        //} BadRequest checks/returns completed
+                    }
+                    //} Reading JSON string + Swapping cups completed
+
+                    // For checking what got passed in, uncomment for use
+                    //return SwapCommands;
+                }
+                return "The final position of the ball is: " + ballLocation;
+            }
+        }
+    }
+
