65194b64503bfb9a159f20ddf209bc03ea70d0a4
mamatha-ISRO <59817698+mamatha-ISRO@users.noreply.github.com>
2022-11-11 13:51:03 -0700

    startup code added

---
+++ abcalbums/Startup.cs
@@ -0,0 +1,41 @@
+﻿namespace abcalbums
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // This is the default https redirection, we are not using that in this coding challenge.
+            //app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            // We are not using Authorization in this coding challenge.
+            //app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
